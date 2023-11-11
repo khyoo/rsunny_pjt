70fd18eb1a45f8821b019f35a70d8083f3742fb8
khyoo0737 <khyoo0737@DESKTOP-OVJ8HMN>
2023-11-12 01:07:15 +0900

    소스 업데이트

---
+++ src/main/resources/static/js/api-key.js
@@ -0,0 +1,5 @@
+// https://www.data.go.kr/
+let data_go_key = 'd6jaYPOmc0Pa1m83x3zueNB3pF6MOKZireJOZo7dkIqOS+D7JakOH1FZjdaUTulBgFjU3TQaaXjFqhgmUomXKA==';
+
+// https://www.vworld.kr/
+let vworld_key = '0500925E-73CA-34F5-82C0-15E11488624A';
